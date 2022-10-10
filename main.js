35c1cdc04b3ff7dd4fc9d4d384f6f016fa8c22d9
André Luis Nascimento Ribeiro <andreluisnr1@hotmail.com>
2022-10-10 09:58:01 -0300

    RocketNFT responsivo

---
+++ main.js
@@ -0,0 +1,7 @@
+function openMenu() {
+  document.getElementById("navigation").classList.add("menu-expanded");
+}
+
+function closeMenu() {
+  document.getElementById("navigation").classList.remove("menu-expanded");
+}
